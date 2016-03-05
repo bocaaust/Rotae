1930bafd9c182f7bae06ccfe6de1abc64ed86c38
Austin Lubetkin <bocaaust@users.noreply.github.com>
2016-03-05 18:02:01 -0500

    Update Main.lua

--- Rotae.codea/Main.lua
+++ Rotae.codea/Main.lua
@@ -1,3 +1,4 @@
+
 -- Rotae
 displayMode(FULLSCREEN)
 -- Use this function to perform your initial setup
@@ -5,6 +6,7 @@ function setup()
     tasks = {11111,22222,3333}
     font("KozGoPro-Light")
     idName = {}
+    input = ""
     idTime = {}
     idName[tasks[1]] = "test"
     idTime[tasks[1]] = 30
@@ -28,14 +30,14 @@ end
 
 -- This function gets called once every frame
 function draw()
-    -- This sets a dark background color 
+    -- This sets a dark background color
     background(253, 202, 150, 255)
-
+    
     --Check if we are on the task screen
     if screen == 0 then
         --points = {vec2(WIDTH/2,HEIGHT/2),vec2(WIDTH*3/4,HEIGHT/2)}
         start = true
-
+        
         --Draw circle with white outline
         stroke(255)
         fill(125)
@@ -58,7 +60,7 @@ function draw()
             textMode(CENTER)
         end
         starti = vec3(0,0,0)
-
+        
         --Check if we have more than one task
         if #tasks > 1 then
             while i <= math.pi*2+.03 do
@@ -67,16 +69,16 @@ function draw()
                     starti.x = i
                     starti.y = starti.y + 1
                     points = {vec2(WIDTH/2,HEIGHT/2),vec2(WIDTH/2+HEIGHT/4*math.cos(i),HEIGHT/2+HEIGHT/4*math.sin(i))}
-
+                    
                     --Pick random seed based off of id
                     math.randomseed(tasks[starti.y])
-
+                    
                     --Pick random color (150-255) is in pastel range
                     temp = color(math.random(150,255),math.random(150,255),math.random(150,255),255)
                     stroke(temp)
                     fill(temp)
                 else
-                --comparison between position difference over total of circle and time section over summation of time
+                    --comparison between position difference over total of circle and time section over summation of time
                     if i-starti.x > 0 then
                         if (i-starti.x)/(math.pi*2) >= (idTime[tasks[starti.y]]/total) then
                             start = true
@@ -84,25 +86,25 @@ function draw()
                         end
                     end
                 end
-                    if start then
-                        --Pick random seed based off of id
-                        math.randomseed(tasks[starti.y])
-
-                        --Pick random color (150-255) is in pastel range
-                        temp = color(math.random(150,255),math.random(150,255),math.random(150,255),255)
-                        stroke(255)
-                        fill(temp)
-                        strokeWidth(5)
-                        line(WIDTH/2,HEIGHT/2,points[2].x,points[2].y)
-                        line(WIDTH/2,HEIGHT/2,WIDTH/2+HEIGHT/4*math.cos(i),HEIGHT/2+HEIGHT/4*math.sin(i))
-                        --line(points[3].x,points[3].y,points[2].x,points[2].y)
-                        fontSize(25)
-                        --fill(math.random(0,255),math.random(0,255),math.random(0,255),255)
-                        --text(idName[tasks[starti.y]],(points[2].x+points[3].x)/2,(points[2].y+points[3].y+points[1].y)/3)
-                    end
+                if start then
+                    --Pick random seed based off of id
+                    math.randomseed(tasks[starti.y])
+                    
+                    --Pick random color (150-255) is in pastel range
+                    temp = color(math.random(150,255),math.random(150,255),math.random(150,255),255)
+                    stroke(255)
+                    fill(temp)
+                    strokeWidth(5)
+                    line(WIDTH/2,HEIGHT/2,points[2].x,points[2].y)
+                    line(WIDTH/2,HEIGHT/2,WIDTH/2+HEIGHT/4*math.cos(i),HEIGHT/2+HEIGHT/4*math.sin(i))
+                    --line(points[3].x,points[3].y,points[2].x,points[2].y)
+                    fontSize(25)
+                    --fill(math.random(0,255),math.random(0,255),math.random(0,255),255)
+                    --text(idName[tasks[starti.y]],(points[2].x+points[3].x)/2,(points[2].y+points[3].y+points[1].y)/3)
+                end
                 --Pick random seed based off of id
                 math.randomseed(tasks[starti.y])
-
+                
                 --Pick random color (150-255) is in pastel range
                 temp = color(math.random(150,255),math.random(150,255),math.random(150,255),255)
                 stroke(temp)
@@ -121,22 +123,22 @@ function draw()
             fontSize(40)
             --text(idName[tasks[1]],WIDTH/2,HEIGHT/2)
         end
-
+        
     end
-
-   --Check if we are on select screen
+    
+    --Check if we are on select screen
     if screen == 1 then
-
+        
         --Check if touch ended
         if CurrentTouch.state == ENDED then
             if touching == 3 then
                 --Process Event
                 table.insert(tasks,math.random(100000,9999999))
                 idTime[tasks[#tasks]] = hours*60
-                screen = 3
+                screen = 2
             end
             touching = 0
-
+            
             --Check flags
             if increase == 1 then
                 --add 30 minute interval
@@ -144,10 +146,10 @@ function draw()
                 --reset flag
                 increase=0
             end
-
+            
             --Decrease is more complicated - we have to check if
-                --the hours is 0.5 or less to avoid negatives
-
+            --the hours is 0.5 or less to avoid negatives
+            
             if decrease == 1 and hours >=0.5 then
                 --get rid of 30 minute interval
                 hours = hours- 0.5
@@ -157,88 +159,94 @@ function draw()
         else
             --Check if we are touching the right half of the screen
             if CurrentTouch.x > WIDTH /2 then
-
+                
                 --Check if we are touching the plus or the minus
                 if CurrentTouch.y > HEIGHT/2 then
                     --"+"
                     touching = 1
-
+                    
                     --Set "increase" flag to ON
                     increase=1
                     decrease=0
                 else
                     -- "-"
                     touching = 2
-
+                    
                     --Set "decrease" flag to ON
                     --"Flags" prevent unexpected results in increase/decrease
                     decrease=1
                     increase=0
                 end
+            elseif CurrentTouch.y <= HEIGHT/4 then
+                    touching = 3
+                end
+                
             end
-    
+            
+            --Set stroke color to white and a thin line
+            stroke(255)
+            strokeWidth(5)
+            
+            --Pastel green fill
+            fill(202,253,150,255)
+            
+            --If touching rectangle, make it darker
+            if touching == 1 then
+                fill(202,253,150,230)
+            end
+            
+            --Plus rectangle
+            rect(WIDTH/2,HEIGHT/2,WIDTH/2,HEIGHT/2)
+            
+            --Pastel green fill
+            fill(202,253,150,255)
+            if touching == 2 then
+                fill(202,253,150,230)
+            end
+            
+            --Minus rectangle
+            rect(WIDTH/2,0,WIDTH/2,HEIGHT/2)
+            
+            --Pastel yellow fill
+            fill(253,253,150,255)
+            if touching == 3 then
+                fill(223, 223, 119, 255)
+            end
+            rect(0,0,WIDTH/2,HEIGHT/4)
+            fill(255)
+            fontSize(70)
+            --Draw instructional text
+            textMode(CORNER)
+            text("Estimate",WIDTH/8,HEIGHT*7.1/8-HEIGHT*.3/8)
+            text("how long",WIDTH/8,HEIGHT*6.4/8-HEIGHT*.3/8)
+            text("your task",WIDTH/8,HEIGHT*5.7/8-HEIGHT*.3/8)
+            text("will take",WIDTH/8,HEIGHT*5/8-HEIGHT*.3/8)
+            
+            --Draw Time
+            textMode(CENTER)
+            text((math.floor(hours*60)).." min.",WIDTH/4,HEIGHT/2.7)
+            textMode(CORNER)
+            
+            -- Draw "DONE"
+            text("DONE",WIDTH/6.8,HEIGHT/11)
+            
+            --Dividing line
+            line(0,HEIGHT/2,WIDTH/2,HEIGHT/2)
+            textMode(CENTER)
+            
+            --Draw +
+            fontSize(180)
+            text("+",WIDTH*3/4,HEIGHT/4*3)
+            
+            --Draw -
+            fontSize(240)
+            text("-",WIDTH*3/4,HEIGHT/4)
+            
         end
-
-        --Set stroke color to white and a thin line
-        stroke(255)
-        strokeWidth(5)
-
-        --Pastel green fill
-        fill(202,253,150,255)
-
-        --If touching rectangle, make it darker
-        if touching == 1 then
-            fill(202,253,150,230)
+        
+        if screen ==2 then
+            
         end
-
-        --Plus rectangle
-        rect(WIDTH/2,HEIGHT/2,WIDTH/2,HEIGHT/2)
-
-        --Pastel green fill
-        fill(202,253,150,255)
-        if touching == 2 then
-            fill(202,253,150,230)
-        end
-
-        --Minus rectangle
-        rect(WIDTH/2,0,WIDTH/2,HEIGHT/2)
-
-        --Pastel yellow fill
-        fill(253,253,150,255)
-        rect(0,0,WIDTH/2,HEIGHT/4)
-        fill(255)
-        fontSize(70)
-        --Draw instructional text
-        textMode(CORNER)
-        text("Estimate",WIDTH/8,HEIGHT*7.1/8-HEIGHT*.3/8)
-        text("how long",WIDTH/8,HEIGHT*6.4/8-HEIGHT*.3/8)
-        text("your task",WIDTH/8,HEIGHT*5.7/8-HEIGHT*.3/8)
-        text("will take",WIDTH/8,HEIGHT*5/8-HEIGHT*.3/8)
-
-        --Draw Time
-        text((hours*60).."min.",WIDTH/6.8,HEIGHT/8)
-
-
-        -- Draw "DONE"
-        text("DONE",WIDTH/6.8,HEIGHT/11)
-
-        --Dividing line
-        line(0,HEIGHT/2,WIDTH/2,HEIGHT/2)
-        textMode(CENTER)
-
-        --Draw +
-        fontSize(180)
-        text("+",WIDTH*3/4,HEIGHT/4*3)
-
-        --Draw -
-        fontSize(240)
-        text("-",WIDTH*3/4,HEIGHT/4)
-
+        
     end
-
-    if screen = 3 then
-
-    end
     
-end
-
